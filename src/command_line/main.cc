be17f9bb5c296645b39876ff60ddedcdc3f6c352
abdchau <abdullahchaudhry0@outlook.com>
2025-09-03 17:57:06 +0000

    Add possibility to run quickjedi

--- src/command_line/main.cc
+++ src/command_line/main.cc
@@ -22,16 +22,22 @@
 #include <iostream>
 #include "node.h"
 #include "string_label.h"
+#include "json_label.h"
 #include "unit_cost_model.h"
 #include "bracket_notation_parser.h"
 #include "apted_tree_index.h"
 #include "quickjedi_index.h"
 
-using Label = label::StringLabel;
-using CostModelLD = cost_model::UnitCostModelLD<Label>;
-using LabelDictionary = label::LabelDictionary<Label>;
+using StringLabel = label::StringLabel;
+using StringCostModelLD = cost_model::UnitCostModelLD<StringLabel>;
+using StringLabelDictionary = label::LabelDictionary<StringLabel>;
+
+using JSONLabel = label::JSONLabel;
+using JSONCostModelLD = cost_model::UnitCostModelJSON<JSONLabel>;
+using JSONLabelDictionary = label::LabelDictionary<JSONLabel>;
 
 int evaluate_apted(std::string, std::string);
+int evaluate_quickjedi(std::string, std::string);
 
 /// Simple command-line tool for executing Tree Edit Distance.
 int main(int argc, char** argv) {
@@ -69,6 +75,8 @@ int main(int argc, char** argv) {
 
   if (std::strcmp(argv[1], "apted") == 0) {
       return evaluate_apted(source_tree_string, dest_tree_string);
+  } else if (std::strcmp(argv[1], "quickjedi") == 0) {
+      return evaluate_quickjedi(source_tree_string, dest_tree_string);
   } else {
       std::cerr << "Incorrect algorithm name. Use apted." << std::endl;
       return -1;
@@ -77,26 +85,55 @@ int main(int argc, char** argv) {
   return 0;
 }
 
+int evaluate_quickjedi(std::string source_tree_string, std::string dest_tree_string) {
+  parser::BracketNotationParser<JSONLabel> bnp;
+  if (!bnp.validate_input(source_tree_string)) {
+    std::cerr << "Incorrect format of source tree. Is the number of opening and closing brackets equal?" << std::endl;
+    return -1;
+  }
+  const node::Node<JSONLabel> source_tree = bnp.parse_single(source_tree_string);
+
+  if (!bnp.validate_input(dest_tree_string)) {
+    std::cerr << "Incorrect format of destination tree. Is the number of opening and closing brackets equal?" << std::endl;
+    return -1;
+  }
+  const node::Node<JSONLabel> destination_tree = bnp.parse_single(dest_tree_string);
+
+  std::cout << "Size of source tree:" << source_tree.get_tree_size() << std::endl;
+  std::cout << "Size of destination tree:" << destination_tree.get_tree_size() << std::endl;
+
+  JSONLabelDictionary ld;
+  JSONCostModelLD ucm(ld);
+  json::QuickJEDITreeIndex<JSONCostModelLD, node::TreeIndexJSON> jedi_algorithm(ucm);
+  node::TreeIndexJSON ti1;
+  node::TreeIndexJSON ti2;
+  node::index_tree(ti1, source_tree, ld, ucm);
+  node::index_tree(ti2, destination_tree, ld, ucm);
+  std::cout << "Distance TED:" << jedi_algorithm.jedi(ti1, ti2) << std::endl;
+
+  return 0;
+}
+
 int evaluate_apted(std::string source_tree_string, std::string dest_tree_string) {
-    parser::BracketNotationParser<Label> bnp;
+    parser::BracketNotationParser<StringLabel> bnp;
     if (!bnp.validate_input(source_tree_string)) {
         std::cerr << "Incorrect format of source tree. Is the number of opening and closing brackets equal?" << std::endl;
         return -1;
     }
-    const node::Node<Label> source_tree = bnp.parse_single(source_tree_string);
+    const node::Node<StringLabel> source_tree = bnp.parse_single(source_tree_string);
 
     if (!bnp.validate_input(dest_tree_string)) {
         std::cerr << "Incorrect format of destination tree. Is the number of opening and closing brackets equal?" << std::endl;
         return -1;
     }
-    const node::Node<Label> destination_tree = bnp.parse_single(dest_tree_string);
+    const node::Node<StringLabel> destination_tree = bnp.parse_single(dest_tree_string);
 
   std::cout << "Size of source tree:" << source_tree.get_tree_size() << std::endl;
   std::cout << "Size of destination tree:" << destination_tree.get_tree_size() << std::endl;
 
-  LabelDictionary ld;
-  CostModelLD ucm(ld);
-  ted::APTEDTreeIndex<CostModelLD, node::TreeIndexAPTED> apted_algorithm(ucm);
+  StringLabelDictionary ld;
+  StringCostModelLD ucm(ld);
+  ted::APTEDTreeIndex<StringCostModelLD, node::TreeIndexAPTED> apted_algorithm(ucm);
   node::TreeIndexAPTED ti1;
   node::TreeIndexAPTED ti2;
   node::index_tree(ti1, source_tree, ld, ucm);
